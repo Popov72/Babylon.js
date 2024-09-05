ec45892e17eb4507fd5c6a99ef106ef726258223
Popov72 <github@evpopov.com>
2024-09-05 14:35:12 +0200

    Various fixes to make it work in WebGPU (WIP)

--- packages/dev/core/src/Rendering/IBLShadows/iblShadowsRenderPipeline.ts
+++ packages/dev/core/src/Rendering/IBLShadows/iblShadowsRenderPipeline.ts
@@ -638,13 +638,20 @@ export class IblShadowsRenderPipeline extends PostProcessRenderPipeline {
 
         // Only turn on the pipeline if the importance sampling RT's are ready
         this._importanceSamplingRenderer.onReadyObservable.add(() => {
-            if (this._voxelRenderer.isReady()) {
-                this.toggleShadow(this._enabled);
-            } else {
-                this._voxelRenderer.onReadyObservable.addOnce(() => {
+            const checkVoxelRendererReady = () => {
+                if (this._voxelRenderer.isReady()) {
                     this.toggleShadow(this._enabled);
-                });
-            }
+                    if (this._enabled) {
+                        this._voxelizationDirty = true;
+                    }
+                } else {
+                    setTimeout(() => {
+                        checkVoxelRendererReady();
+                    }, 16);
+                }
+            };
+
+            checkVoxelRendererReady();
         });
     }
 
@@ -700,9 +707,6 @@ export class IblShadowsRenderPipeline extends PostProcessRenderPipeline {
             }
         });
         this._shadowCompositePP._prePassEffectConfiguration = this._prePassEffectConfiguration;
-        setTimeout(() => {
-            this._voxelizationDirty = true;
-        }, 500);
     }
 
     private _createEffectPasses(cameras: Camera[] | undefined) {
